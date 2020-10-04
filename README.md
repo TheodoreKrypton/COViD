74326505692efe74e438f933239f09e802368770
Wheat Carrier <wheatcarrier@gmail.com>
2020-10-04 21:06:32 +0800

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,25 @@ COCO Visual Demonstrator - Web frontend visualizer for COCO format annotations
 
 ## DEMO
 https://theodorekrypton.github.io/COViD/index.html
+
+
+## Usage
+HTML
+```html
+<script src="https://raw.githubusercontent.com/TheodoreKrypton/COViD/master/index.js">
+<canvas id="image" width="600" height="600"></canvas>
+```
+
+Javascript
+```javascript
+var annotation = {
+  "segmentation": [[376.97, 176.91, 398.81, 176.91, 396.38, 147.78, 447.35, 146.17, 448.16, 172.05, 448.16, 178.53, 464.34, 186.62, 464.34, 192.28, 448.97, 195.51, 447.35, 235.96, 441.69, 258.62, 454.63, 268.32, 462.72, 276.41, 471.62, 290.98, 456.25, 298.26, 439.26, 292.59, 431.98, 308.77, 442.49, 313.63, 436.02, 316.86, 429.55, 322.53, 419.84, 354.89, 402.04, 359.74, 401.24, 312.82, 370.49, 303.92, 391.53, 299.87, 391.53, 280.46, 385.06, 278.84, 381.01, 278.84, 359.17, 269.13, 373.73, 261.85, 374.54, 256.19, 378.58, 231.11, 383.44, 205.22, 385.87, 192.28, 373.73, 184.19]],
+  "iscrowd": 0,
+  "bbox":[359.17, 146.17, 112.45, 213.57]
+};
+var imageUrl = "http://farm3.staticflickr.com/2407/2477059973_870efb557b_z.jpg";
+
+covid.drawNoncrowd(document.getElementById("image"), image, annotation);
+```
+
+![result](https://i.imgur.com/JEFH2Ag.png)
--- index.js
+++ index.js
@@ -1,195 +1,204 @@
-function randNumber() {
-  return Math.floor(Math.random() * 255);
-}
-
-function getRandomColor() {
-  return `rgba(${randNumber()},${randNumber()},${randNumber()},0.5)`;
-}
-
-function indexToXY(index, h) {
-  var x = Math.floor(index / h);
-  var y = index - x * h;
-  return [x, y]
-}
-
-function mapX(x, params) {
-  return x * params.ratio + params.dx;
-}
-
-function mapY(y, params) {
-  return y * params.ratio + params.dy
-}
-
-function mapXY(x, y, params) {
-  return [mapX(x, params), mapY(y, params)];
-}
-
-function getResizeParams(canvas, { height, width }) {
-  var params = {
-    h: height,
-    w: width,
-  };
-
-  if (params.h > params.w) {
-    params.ratio = canvas.height / params.h;
-    params.dHeight = canvas.height;
-    params.dWidth = Math.floor(params.ratio * params.w);
-    params.dy = 0;
-    params.dx = Math.floor((canvas.width - params.dWidth) / 2);
-  } else {
-    params.ratio = canvas.width / params.w;
-    params.dWidth = canvas.width;
-    params.dHeight = Math.floor(params.ratio * params.h);
-    params.dx = 0;
-    params.dy = Math.floor((canvas.height - params.dHeight) / 2);
-  }
-
-  return params;
-};
-
-
-function rleFill(startIndex, offset, h, context, params) {
-  var xy = indexToXY(startIndex, h);
-  var x1 = xy[0];
-  var y1 = xy[1];
-
-  var mapped = {};
-  mapped.x1 = mapX(x1, params);
-  mapped.y1 = mapY(y1, params);
-  context.moveTo(mapped.x1, mapped.y1);
-  if (y1 + offset <= h) {
-    context.lineTo(mapped.x1, mapY(y1 + offset, params));
-  } else {
-    var x2y2 = indexToXY(startIndex + offset, h);
-    var x2 = x2y2[0];
-    var y2 = x2y2[1];
-    mapped.top = params.dy;
-    mapped.bottom = params.dHeight + params.dy;
-    context.lineTo(x1, mapped.bottom);
-    for (var i = x1 + 1; i < x2; i++) {
-      context.moveTo(i, mapped.top);
-      context.lineTo(i, mapped.bottom);
+var covid =
+  (function () {
+    function randNumber() {
+      return Math.floor(Math.random() * 255);
     }
-    mapped.x2 = mapX(x2, params);
-    context.moveTo(mapped.x2, mapped.top);
-    context.lineTo(mapped.x2, y2);
-  }
-}
 
-function drawBbox(bbox, params, context) {
-  var x = mapX(bbox[0], params);
-  var y = mapY(bbox[1], params);
-  var width = bbox[2] * params.ratio;
-  var height = bbox[3] * params.ratio;
+    function getRandomColor() {
+      return `rgba(${randNumber()},${randNumber()},${randNumber()},0.5)`;
+    }
 
-  context.beginPath();
-  context.rect(x, y, width, height);
-  context.stroke();
-}
+    function indexToXY(index, h) {
+      var x = Math.floor(index / h);
+      var y = index - x * h;
+      return [x, y]
+    }
 
-function drawPolygon(polygon, params, context) {
-  var region = new Path2D();
-  region.moveTo(mapX(polygon[0], params), mapY(polygon[1], params));
-  for (var i = 2; i < polygon.length; i += 2) {
-    region.lineTo(mapX(polygon[i], params), mapY(polygon[i + 1], params));
-  }
-  region.lineTo(mapX(polygon[0], params), mapY(polygon[1], params));
-  region.closePath();
-  context.fill(region);
-}
+    function mapX(x, params) {
+      return x * params.ratio + params.dx;
+    }
 
-function drawRLE(rle, params, context) {
-  context.beginPath();
-  context.moveTo(...mapXY(...indexToXY(rle[0], params.h), params));
-  var offset = rle[0];
-  for (var i = 1; i < rle.length; i += 2) {
-    rleFill(offset, rle[i], params.h, context, params);
-    offset += rle[i] + rle[i + 1];
-  }
-  rleFill(offset, rle[rle.length - 1], params.h, context, params);
-  context.stroke();
-}
+    function mapY(y, params) {
+      return y * params.ratio + params.dy
+    }
 
-function draw(canvas, image, annotation) {
-  var ctx = canvas.getContext('2d');
+    function mapXY(x, y, params) {
+      return [mapX(x, params), mapY(y, params)];
+    }
 
-  var params = getResizeParams(canvas, image);
-  ctx.drawImage(image, params.dx, params.dy, params.dWidth, params.dHeight);
+    function getResizeParams(canvas, { height, width }) {
+      var params = {
+        h: height,
+        w: width,
+      };
 
-  if (annotation.bbox) {
-    ctx.strokeStyle = "lime";
-    ctx.lineWidth = "3";
-    drawBbox(annotation.bbox, params, ctx);
-  }
-
-  if (annotation.segmentation) {
-    if (annotation.iscrowd === 0) {
-      ctx.lineWidth = "1";
-      ctx.fillStyle = getRandomColor();
-      for (var i = 0; i < annotation.segmentation.length; i++) {
-        drawPolygon(annotation.segmentation[i], params, ctx);
+      if (params.h > params.w) {
+        params.ratio = canvas.height / params.h;
+        params.dHeight = canvas.height;
+        params.dWidth = Math.floor(params.ratio * params.w);
+        params.dy = 0;
+        params.dx = Math.floor((canvas.width - params.dWidth) / 2);
+      } else {
+        params.ratio = canvas.width / params.w;
+        params.dWidth = canvas.width;
+        params.dHeight = Math.floor(params.ratio * params.h);
+        params.dx = 0;
+        params.dy = Math.floor((canvas.height - params.dHeight) / 2);
       }
-    } else {
-      ctx.strokeStyle = getRandomColor();
-      drawRLE(annotation.segmentation.counts, params, ctx)
-    }
-  }
-}
 
-function uncompressRLE(rle) {
-  let m = 0;
-  let p = 0;
-  let k = 0;
-  let x = 0;
-  let more = true;
-  let cnts = new Uint32Array(rle.length);
+      return params;
+    };
 
-  while (rle[p]) {
-    x = 0;
-    k = 0;
-    more = true;
-    while (more) {
-      let c = rle.charCodeAt(p) - 48;
-      x |= (c & 0x1F) << 5 * k;
-      more = c & 0x20;
-      p++;
-      k++;
-      if (!more && (c & 0x10)) {
-        x |= -1 << 5 * k;
+
+    function rleFill(startIndex, offset, h, context, params) {
+      var xy = indexToXY(startIndex, h);
+      var x1 = xy[0];
+      var y1 = xy[1];
+
+      var mapped = {};
+      mapped.x1 = mapX(x1, params);
+      mapped.y1 = mapY(y1, params);
+      context.moveTo(mapped.x1, mapped.y1);
+      if (y1 + offset <= h) {
+        context.lineTo(mapped.x1, mapY(y1 + offset, params));
+      } else {
+        var x2y2 = indexToXY(startIndex + offset, h);
+        var x2 = x2y2[0];
+        var y2 = x2y2[1];
+        mapped.top = params.dy;
+        mapped.bottom = params.dHeight + params.dy;
+        context.lineTo(x1, mapped.bottom);
+        for (var i = x1 + 1; i < x2; i++) {
+          context.moveTo(i, mapped.top);
+          context.lineTo(i, mapped.bottom);
+        }
+        mapped.x2 = mapX(x2, params);
+        context.moveTo(mapped.x2, mapped.top);
+        context.lineTo(mapped.x2, y2);
       }
     }
-    if (m > 2) {
-      x += cnts[m - 2];
+
+    function drawBbox(bbox, params, context) {
+      var x = mapX(bbox[0], params);
+      var y = mapY(bbox[1], params);
+      var width = bbox[2] * params.ratio;
+      var height = bbox[3] * params.ratio;
+
+      context.beginPath();
+      context.rect(x, y, width, height);
+      context.stroke();
     }
-    cnts[m++] = x;
-  }
-  return cnts;
-}
 
-function drawNoncrowd(canvas, url, ann) {
-  const image = document.createElement("img");
-  image.style.display = "none";
-  image.onload = function (e) {
-    draw(canvas, image, ann);
-  }
-  image.src = url;
-}
+    function drawPolygon(polygon, params, context) {
+      var region = new Path2D();
+      region.moveTo(mapX(polygon[0], params), mapY(polygon[1], params));
+      for (var i = 2; i < polygon.length; i += 2) {
+        region.lineTo(mapX(polygon[i], params), mapY(polygon[i + 1], params));
+      }
+      region.lineTo(mapX(polygon[0], params), mapY(polygon[1], params));
+      region.closePath();
+      context.fill(region);
+    }
 
-function drawUncompressed(canvas, url, ann) {
-  const image = document.createElement("img");
-  image.style.display = "none";
-  image.onload = function (e) {
-    draw(canvas, image, ann);
-  }
-  image.src = url;
-}
+    function drawRLE(rle, params, context) {
+      context.beginPath();
+      context.moveTo(...mapXY(...indexToXY(rle[0], params.h), params));
+      var offset = rle[0];
+      for (var i = 1; i < rle.length; i += 2) {
+        rleFill(offset, rle[i], params.h, context, params);
+        offset += rle[i] + rle[i + 1];
+      }
+      rleFill(offset, rle[rle.length - 1], params.h, context, params);
+      context.stroke();
+    }
 
-function drawCompressed(canvas, url, ann) {
-  const image = document.createElement("img");
-  image.style.display = "none";
-  image.onload = function (e) {
-    ann.segmentation.counts = uncompressRLE(ann.segmentation.counts);
-    draw(canvas, image, ann);
-  }
-  image.src = url;
-}
+    function draw(canvas, image, annotation) {
+      var ctx = canvas.getContext('2d');
+
+      var params = getResizeParams(canvas, image);
+      ctx.drawImage(image, params.dx, params.dy, params.dWidth, params.dHeight);
+
+      if (annotation.bbox) {
+        ctx.strokeStyle = "lime";
+        ctx.lineWidth = "3";
+        drawBbox(annotation.bbox, params, ctx);
+      }
+
+      if (annotation.segmentation) {
+        if (annotation.iscrowd === 0) {
+          ctx.lineWidth = "1";
+          ctx.fillStyle = getRandomColor();
+          for (var i = 0; i < annotation.segmentation.length; i++) {
+            drawPolygon(annotation.segmentation[i], params, ctx);
+          }
+        } else {
+          ctx.strokeStyle = getRandomColor();
+          drawRLE(annotation.segmentation.counts, params, ctx)
+        }
+      }
+    }
+
+    function uncompressRLE(rle) {
+      let m = 0;
+      let p = 0;
+      let k = 0;
+      let x = 0;
+      let more = true;
+      let cnts = new Uint32Array(rle.length);
+
+      while (rle[p]) {
+        x = 0;
+        k = 0;
+        more = true;
+        while (more) {
+          let c = rle.charCodeAt(p) - 48;
+          x |= (c & 0x1F) << 5 * k;
+          more = c & 0x20;
+          p++;
+          k++;
+          if (!more && (c & 0x10)) {
+            x |= -1 << 5 * k;
+          }
+        }
+        if (m > 2) {
+          x += cnts[m - 2];
+        }
+        cnts[m++] = x;
+      }
+      return cnts;
+    }
+
+    function drawNoncrowd(canvas, url, ann) {
+      const image = document.createElement("img");
+      image.style.display = "none";
+      image.onload = function (e) {
+        draw(canvas, image, ann);
+      }
+      image.src = url;
+    }
+
+    function drawUncompressed(canvas, url, ann) {
+      const image = document.createElement("img");
+      image.style.display = "none";
+      image.onload = function (e) {
+        draw(canvas, image, ann);
+      }
+      image.src = url;
+    }
+
+    function drawCompressed(canvas, url, ann) {
+      const image = document.createElement("img");
+      image.style.display = "none";
+      image.onload = function (e) {
+        ann.segmentation.counts = uncompressRLE(ann.segmentation.counts);
+        draw(canvas, image, ann);
+      }
+      image.src = url;
+    }
+
+    return {
+      drawNoncrowd,
+      drawUncompressed,
+      drawCompressed,
+    }
+  })();
